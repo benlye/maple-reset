73f134ac82a7b82fbc32696401c3f0242da8cc95
Ben Lye <ben@lye.co.nz>
2019-07-01 13:14:32 +0100

    Initial check-in

---
+++ source/maple-reset/maple-reset.cpp
@@ -0,0 +1,65 @@
+// maple-reset.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include "pch.h"
+#include "windows.h"
+#include <conio.h>
+#include <stdio.h>
+
+int main(int argc, char* argv[])
+{
+	HANDLE hComm;
+	
+	printf("maple-reset 0.1\n");
+	printf("This program is Free Sofware and has NO WARRANTY\n\n");
+	printf("https:\/\/github.com\/benlye\/maple-reset\n\n");
+
+	if (argc == 1) 
+	{
+		fprintf(stderr, "ERROR: Port not specified\n", argv[1]);
+		fprintf(stderr, "\nUsage: maple-reset.exe COM3\n", argv[1]);
+		return -1;
+	}
+
+	hComm = CreateFileA(argv[1], GENERIC_READ | GENERIC_WRITE,
+		0,
+		0,
+		OPEN_EXISTING,
+		NULL,
+		0);
+
+	if (hComm == INVALID_HANDLE_VALUE)
+	{
+		fprintf(stderr, "ERROR: Failed to open %s\n", argv[1]);
+		return -1;
+	}
+
+	char bytes_to_send[4];
+	bytes_to_send[0] = 0x31;	// 1
+	bytes_to_send[1] = 0x45;	// E
+	bytes_to_send[2] = 0x41;	// A
+	bytes_to_send[3] = 0x46;	// F
+
+	DWORD bytes_written, total_bytes_written = 0;
+
+	if (EscapeCommFunction(hComm, SETDTR) == 0) {
+		fprintf(stderr, "ERROR: Failed to set DTR\n");
+		return 1;
+	}
+	
+	if (EscapeCommFunction(hComm, CLRDTR) == 0) {
+		fprintf(stderr, "ERROR: Failed to clear DTR\n");
+		return 2;
+	}
+
+	if (!WriteFile(hComm, bytes_to_send, 4, &bytes_written, NULL))
+	{
+		fprintf(stderr, "ERROR: Failed to send string.\n");
+		CloseHandle(hComm);
+		return 3;
+	}
+
+	fprintf(stdout, "Reset sequence sent to %s.\n", argv[1]);
+
+	return 0;
+}
